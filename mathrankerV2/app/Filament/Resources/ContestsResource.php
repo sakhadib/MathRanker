4cc16bc53a3cc15d61fc46ee99468764e141fcd7
takiafarhin <takiafarhin@iut-dhaka.edu>
2024-05-31 22:15:48 +0600

    contest resource created

---
+++ mathrankerV2/app/Filament/Resources/ContestsResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContestsResource\Pages;
+use App\Filament\Resources\ContestsResource\RelationManagers;
+use App\Models\Contests;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ContestsResource extends Resource
+{
+    protected static ?string $model = Contests::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContests::route('/'),
+            'create' => Pages\CreateContests::route('/create'),
+            'edit' => Pages\EditContests::route('/{record}/edit'),
+        ];
+    }
+}
